7381943446dcea5f91937515b448405225ebf746
Andrew Ferk <andrewferk@gmail.com>
2012-08-16 00:04:47 -0500

    Setup webserver for vagrant, and change proxy location. * Webserver documet root is /vagrant. * Webserver log root is /vagrant/logs. * /ad/ is proxied to Ackmann Dickenson.

--- cookbooks/apache2_proxy/recipes/default.rb
+++ cookbooks/apache2_proxy/recipes/default.rb
@@ -2,6 +2,11 @@ include_recipe "apache2"
 apache_module "proxy" do
   conf true
 end
+web_app "web_app" do
+  server_name "localhost"
+  docroot "/vagrant"
+  logroot "/vagrant/logs"
+end
 include_recipe "apache2::mod_proxy_http"
 include_recipe "apache2::mod_headers"
 include_recipe "apache2::mod_deflate"
